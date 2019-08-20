67e7de33bcf6396662d38cba448e1b673f99ccf6
Frederik Hvilshoj <fhvilshoj@cs.au.dk>
2019-08-20 17:14:07 +0200

    tmp commit to move work to home

--- invtf/approximation_layers.py
+++ invtf/approximation_layers.py
@@ -1,6 +1,8 @@
+import sys
 import os 
 import tensorflow as tf
 import tensorflow.keras as keras 
+from tensorflow.math import l2_normalize as normalize
 import numpy as np
 from tensorflow.keras.layers import ReLU, Conv2D
 from invtf.override import print_summary
@@ -8,10 +10,55 @@ from invtf.coupling_strategy import *
 
 
 class SpectralNormalizationConstraint(keras.constraints.Constraint):
-	def __call__(self, w, n_power_iter=5):
+	def __init__(self, coeff, input_shape, n_power_iter=5, strides=None, padding=None, **kwargs):
+		"""
+			Arguments:
+				coeff:			Contraction coefficient of the conv (iResNet default is 0.9)
+				input_shape:	Input_shape of the layer to be normalized
+				n_power_iter:	Number of power iterations to estimate largest eigenvalue (iResNet default is 0.9)
+		"""
 		# TODO do spectral normalization.
 		# Not sure how many power iterations is needed? 5 is default in iResNet.
-		return w
+		super(SpectralNormalizationConstraint, self).__init__(**kwargs)
+		self.coeff = coeff
+		self.n_power_iter = n_power_iter if n_power_iter >= 0 else 5
+		self.input_shape = input_shape
+		if strides and padding:
+			self.strides = strides
+			self.padding = padding
+
+	def __call__(self, w):
+		if self.strides: # Conv
+			input_shape		= [1] + [int(i) for i in self.input_shape[1:]]
+
+			# initialize u and v to norm 1 random vectors
+			v				= normalize(tf.random.normal((tf.reduce_prod(input_shape),)))
+
+			tmp				= tf.nn.conv2d(tf.reshape(v, input_shape), w, strides=self.strides, padding=self.padding)
+			output_shape	= tmp.shape
+
+			u				= normalize(tf.random.normal((tf.reduce_prod(output_shape),)))
+
+			inp		= lambda v: tf.reshape(v, input_shape)
+			out		= lambda u: tf.reshape(u, output_shape)
+			flat	= lambda x: tf.reshape(x, [-1])
+			params	= {'strides': self.strides, 'padding': self.padding}
+			
+			# Power iteration
+			for _ in range(self.n_power_iter):
+				v_s	= tf.nn.conv2d_transpose(out(u), w, output_shape=input_shape, **params)
+				v	= normalize(flat(v))
+
+				u_s = tf.nn.conv2d(inp(v), w, **params)
+				u	= normalize(flat(u))
+
+			# Normalization
+			vW		= tf.nn.conv2d(inp(v), w, **params)
+			sigma	= tf.tensordot(flat(vW), u, 1)
+			factor	= tf.maximum(1., sigma / self.coeff)
+			
+		res = w / (factor + 1e-5) # Stability
+		return res
 
 class ResidualBlock(keras.layers.Layer):
 	"""
@@ -34,19 +81,19 @@ class ResidualBlock(keras.layers.Layer):
 
 	def add(self, layer): self.layers.append(layer)
 
-	def build(self, input_shape):
+	def build(self, input_shape, *args, **kwargs):
 		out_dim = input_shape
 		for layer in self.layers:
 			layer.build(input_shape=out_dim)
 			out_dim = layer.compute_output_shape(input_shape=out_dim)
 
-		super(ResidualBlock, self).build(*args, **kwargs)
+		super(ResidualBlock, self).build(input_shape, *args, **kwargs)
 		self.built = True
 
 	def _call(self, X):
 		out = X
 		for layer in self.layers:
-			out = layer.call(out)
+			out = layer(out)
 		return out
 
 	def call(self, X):
@@ -57,8 +104,8 @@ class ResidualBlock(keras.layers.Layer):
 		# TODO: no stopping early here.  Should there be some threshold on how
 		# much the solution changes?
 		maxIter = 100 # Arbitrary max iteration for inverse (choosen equal to default in iResNet # TODO Make dynamic
-		X = Z
-		for i in arange(maxIter):
+		X		= Z
+		for i in range(maxIter):
 			gx = self._call(X)
 			X = Z - gx
 		return X
@@ -84,8 +131,8 @@ class ResidualBlock(keras.layers.Layer):
 			# Due to chain rule, taking gradient of W is the same as wT @ J
 			W		= [tf.gradients(Z, X, grad_ys=W[:,i]) for i in range(N)]
 			W		= tf.stack(W, axis=1)
-			wT_J	= grads.reshape(self.input_shape[0], N, 1, -1)
-			vFlat	= v.reshape(self.input_shape[0], N, -1, 1)
+			wT_J	= tf.reshape(grads, self.input_shape[0], N, 1, -1)
+			vFlat	= tf.reshape(v, self.input_shape[0], N, -1, 1)
 
 			# Shape (b, N, 1)
 			product	= wT_J @ vFlat / float(k) 
--- test/residual_network.py
+++ test/residual_network.py
@@ -1,7 +1,15 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
 import unittest
 import tensorflow as tf
 import tensorflow.keras as keras
+from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.math import l2_normalize as normalize
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,40 +19,91 @@ sys.path.append("../")
 from invtf.approximation_layers import ResidualBlock, SpectralNormalizationConstraint
 
 class ResidualNetworkTest(unittest.TestCase):
-	X = tf.constant(keras.datasets.cifar10.load_data()[0][0][:10].astype('f')) # a single cifar image batch.
+	X = tf.constant(keras.datasets.cifar10.load_data()[0][0][:100].astype('f')) # a single cifar image batch.
 
-	def assertInverse(self, g, X): 
-		"""
-			Input: 		
-				g:		Model which has call(X) return a tensor that depends on X. 
-				X:		Test data. 
-	
-			Computes the reconstruction of X and compares with X. 
+	def get_norm(self, w):
+		input_shape		= [1] + [int(i) for i in self.X.shape[1:]]
 
-		""" 
-		rec = g.call_inv(X)
-		A = np.allclose(rec, X, atol=1, rtol=0.1) # assumes data is in bytes. 
+		# initialize u and v to norm 1 random vectors
+		v				= normalize(tf.random.normal((tf.reduce_prod(input_shape),)))
 
-		# find entry with largest difference and print their relative values. 
-		diff = np.abs(A - rec)
-		entry = np.argmax(diff)
+		tmp				= tf.nn.conv2d(tf.reshape(v, input_shape), w, strides=[1,1], padding='SAME')
+		output_shape	= tmp.shape
 
-		if A is False: 
-			fig, ax = plt.subplots(3, 1)
-			ax[0].imshow(rec.reshape(32,32,3)/255)
-			ax[1].imshow(X.reshape(32,32,3)/255)
-			ax[2].imshow((rec-X).reshape(32,32,3)/255)
-			plt.show()
+		u				= normalize(tf.random.normal((tf.reduce_prod(output_shape),)))
 
-		self.assertTrue(A)
+		inp		= lambda v: tf.reshape(v, input_shape)
+		out		= lambda u: tf.reshape(u, output_shape)
+		flat	= lambda x: tf.reshape(x, [-1])
+		params	= {'strides': [1,1], 'padding': 'SAME'}
 		
+		# Power iteration
+		for _ in range(5):
+			v_s	= tf.nn.conv2d_transpose(out(u), w, output_shape=input_shape, **params)
+			v	= normalize(flat(v))
+
+			u_s = tf.nn.conv2d(inp(v), w, **params)
+			u	= normalize(flat(u))
+
+		# Normalization
+		vW		= tf.nn.conv2d(inp(v), w, **params)
+		sigma	= tf.tensordot(flat(vW), u, 1)
+		factor	= sigma / 0.9 # tf.maximum(1., sigma / 0.9)
+		return sigma, factor
+
+
 	def test_inverse(self):
 		r = ResidualBlock()
-		#																	( b,     h,      w,     c )
-		r.add(Conv2D(3, kernel_size=(3,3), activation="relu",      padding='same',
-						kernel_constraint=SpectralNormalizationConstraint())) 
 
-		y = r.call(self.X)
-		self.assertInverse(r, y)
+		sn = SpectralNormalizationConstraint(0.9, self.X.shape, n_power_iter=5, strides=[1,1], padding='SAME')
+		r.add(Conv2D(3, kernel_size=(3,3), activation='elu',      padding='SAME', strides=[1,1], kernel_constraint=sn)) 
+		sn = SpectralNormalizationConstraint(0.9, self.X.shape, n_power_iter=5, strides=[1,1], padding='SAME')
+		r.add(Conv2D(1, kernel_size=(3,3), activation='elu',      padding='SAME', strides=[1,1], kernel_constraint=sn)) 
+		sn = SpectralNormalizationConstraint(0.9, [int(i) for i in self.X.shape[:-1]] + [1], n_power_iter=5, strides=[1,1], padding='SAME')
+		r.add(Conv2D(3, kernel_size=(3,3), activation=None,      padding='SAME', strides=[1,1], kernel_constraint=sn)) 
 
+		s = Sequential()
+		s.add(r)
+		optimizer = Adam()
+		loss = MeanSquaredError()
+		s.compile(optimizer=optimizer, loss=loss)
+		Z = s.predict(self.X)
+
+		print('\n\n\n - - -  \n\n\n')
+		print(self.get_norm(r.layers[0].kernel))
+		print('\n\n\n - - -  \n\n\n')
+
+		s.fit(self.X, self.X, epochs=10)
+
+		Xr = r.call_inv(Z)
+		A = np.allclose(self.X, Xr, atol=1e-3, rtol=1e-3)
+
+		print('\n\n\n - - -  \n\n\n')
+		print(self.get_norm(r.layers[0].kernel))
+		print('\n\n\n - - -  \n\n\n')
+
+		# Plotting stuff
+		if not A:
+			rows=3
+			fig, ax = plt.subplots(rows + 1, 4)
+			ax[0, 0].set_title('X')
+			ax[0, 1].set_title('Rec')
+			ax[0, 2].set_title('Diff')
+			ax[0, 3].set_title('Z')
+			for i in range(rows):
+				ax[i, 0].imshow(tf.reshape(self.X[i], (32, 32, 3)) / 255.)
+				ax[i, 1].imshow(tf.reshape(Xr[i], (32, 32, 3)) / 255.)
+				ax[i, 2].imshow(tf.reshape(Xr[i] - self.X[i], (32, 32, 3)) / 255.)
+				ax[i, 3].imshow(tf.reshape(Z[i], (32, 32, 3)) / 255.)
+
+			entry = tf.reduce_max(tf.abs(self.X - Xr))
+			truth = tf.equal(tf.abs(self.X - Xr), entry)
+			i = tf.argmax(tf.cast([tf.reduce_any(truth[i]) for i in range(len(self.X))], tf.float32))
+			ax[rows, 0].imshow(tf.reshape(self.X[i], (32, 32, 3)) / 255.)
+			ax[rows, 1].imshow(tf.reshape(Xr[i], (32, 32, 3)) / 255.)
+			ax[rows, 2].imshow(tf.reshape(Xr[i] - self.X[i], (32, 32, 3)) / 255.)
+			ax[rows, 3].imshow(tf.reshape(Z[i], (32, 32, 3)) / 255.)
+			plt.show()
+
+		assert A, tf.reduce_max(tf.abs(self.X - Xr))
 
