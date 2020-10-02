Fixed an issue with latent distribution variance that broke training, now seems to work. Also changed name from 'norm' to 'normalization' for better readability.

invtf/__init__.py
@@ -361,14 +361,13 @@ class Generator(keras.Model):
 				Alternatively, one could have scaled log_det by the size of the mini-batch. 
 			
 		"""
-
-		d			= tf.cast(tf.reduce_prod(y_pred.shape[1:]), 		tf.float32)
-		norm		= d * np.log(2.).astype(np.float32) 
+		d				= tf.cast(tf.reduce_prod(y_pred.shape[1:]), 		tf.float32)
+		normalization	= d * np.log(2.).astype(np.float32) 
 
 		# Divide by /d to get per dimension. 
 		# Divide by log(2) to go from log base E (default in tensorflow) to log base 2. 
 		# https://www.tensorflow.org/versions/r2.0/api_docs/python/tf/math/log
-		normal 		= self.latent.log_density(y_pred) / norm 
+		normal 		= self.latent.log_density(y_pred) / normalization
 		batch_size 	= tf.cast(tf.shape(y_pred)[0], 	tf.float32)
 		normal		= normal / batch_size
 		return 		- normal

invtf/models.py
@@ -5,6 +5,8 @@ from invtf.dequantize 	import *
 from tensorflow			import keras
 from tensorflow.keras 	import Sequential
 
+from invtf.coupling_layers import * 
+
 from tensorflow.keras.layers import ReLU, Dense, Flatten, Reshape, Conv2D
 from tensorflow.keras.models import Sequential
 
@@ -64,8 +66,8 @@ class NICE():
 		n, d = X.shape
 
 		# This differs from MNIST to CIFAR in original article. 
-		#g = invtf.Generator(latent.Logistic(d))  
-		g = invtf.Generator(latent.Normal(d))  
+		#g = invtf.Generator(latent.Logistic())  
+		g = invtf.Generator(latent.Normal())   
 
 		# Pre-process steps. 
 		g.add(UniformDequantize	(input_shape=[d])) 
@@ -119,7 +121,7 @@ class RealNVP():
 		input_shape = X.shape[1:]
 		d 			= np.prod(input_shape)
 
-		g = invtf.Generator(latent.Normal(d)) 
+		g = invtf.Generator(latent.Normal()) 
 
 		# Pre-process steps. 
 		g.add(UniformDequantize	(input_shape=input_shape)) 
@@ -232,7 +234,7 @@ class Glow():
 		input_shape = X.shape[1:]
 		d 			= np.prod(input_shape)
 
-		g = invtf.Generator(latent.Normal(d)) 
+		g = invtf.Generator(latent.Normal()) 
 
 		# Pre-process steps. 
 		g.add(keras.layers.InputLayer(input_shape=input_shape))